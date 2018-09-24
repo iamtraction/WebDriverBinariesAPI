46d9af65498748920a3484afb6a7a95458b9c862
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-09-24 14:38:18 +0000

    scripts: add geckodriver download script

---
+++ scripts/firefox.py
@@ -0,0 +1,22 @@
+import json
+from urllib import request
+
+
+# Get the latest build name & download links of Firefox WebDriver
+base_url = 'https://api.github.com/repos/'
+latest_release_path = '{0}mozilla/geckodriver/releases/latest'.format(base_url)
+
+data = request.urlopen(latest_release_path)
+data = json.load(data)
+
+builds = data['assets']
+
+
+# Download every build
+assets_dir = "./assets/geckodriver"
+
+for build in builds:
+    request.urlretrieve(
+        build['browser_download_url'],
+        "{0}/{1}".format(assets_dir, build['name'])
+    )
