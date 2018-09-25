fd3fb1cf7dba29d99e340245e5589c006857ce7c
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-09-25 01:53:14 +0000

    app: add main file

---
+++ source/app.ts
@@ -0,0 +1,97 @@
+"use strict";
+
+import * as bodyParser from "body-parser";
+import * as express from "express";
+import * as logger from "morgan";
+
+import * as IndexRoute from "./routes/index";
+
+/**
+ * The server.
+ *
+ * @class Server
+ */
+class Server {
+  public app: express.Application;
+
+  /**
+   * Bootstrap the application.
+   *
+   * @class Server
+   * @method bootstrap
+   * @static
+   */
+  public static bootstrap(): Server {
+    return new Server();
+  }
+
+  /**
+   * Constructor.
+   *
+   * @class Server
+   * @constructor
+   */
+  constructor() {
+    // Create Express.js application
+    this.app = express();
+
+    // Configure application
+    this.config();
+
+    // Configure routes
+    this.routes();
+  }
+
+  /**
+   * Configure application
+   *
+   * @class Server
+   * @method config
+   * @return void
+   */
+  private config() {
+    // Use logger for server side logging
+    this.app.use(logger("dev"));
+
+    // Mount json form parser
+    this.app.use(bodyParser.json());
+
+    // Mount query string parser
+    this.app.use(bodyParser.urlencoded({ extended: true }));
+  }
+
+  /**
+   * Configure routes
+   *
+   * @class Server
+   * @method routes
+   * @return void
+   */
+  private routes() {
+    // Get router
+    let router: express.Router;
+    router = express.Router();
+
+    // Create routes
+    const Index: IndexRoute.Index = new IndexRoute.Index();
+
+    // Routes
+    router.get("/", Index.main.bind(Index.main));
+
+    // Use router middleware
+    this.app.use(router);
+
+    // Catch 404 and forward to error handler
+    this.app.use((_req: express.Request, res: express.Response, next: express.NextFunction) => {
+      res.json({
+        error: "404",
+        message: "Not Found"
+      });
+
+      next();
+    });
+  }
+}
+
+const server = Server.bootstrap();
+export = server.app;
